4dd43a5500bf6a9d5370adfa9442d31ec5b5ba0b
Beinan <beinan@alluxio.com>
2023-04-24 00:42:01 -0700

    Code clean up

--- src/settings.rs
+++ src/settings.rs
@@ -3,7 +3,6 @@ use lazy_static::lazy_static;
 
 use config::{Config, ConfigError, File};
 use serde_derive::Deserialize;
-use std::env;
 
 lazy_static! {
     pub static ref SETTINGS: Settings = Settings::new().unwrap();
@@ -21,11 +20,9 @@ pub struct Settings {
 
 impl Settings {
     pub fn new() -> Result<Self, ConfigError> {
-        let run_mode = env::var("RUN_MODE").unwrap_or_else(|_| "development".into());
         let s = Config::builder()
             .add_source(File::with_name("fairy_config"))
             .build()?;
-        // println!("load settings: {:?}", s);
         s.try_deserialize()
     }
 }
