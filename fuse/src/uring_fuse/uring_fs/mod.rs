b57691483d43ffc907e64077c8a9bf5dc44c3137
beinan <soros.wang@yahoo.com>
2023-11-14 23:06:55 -0800

    Fix file handle for create

--- fuse/src/uring_fuse/uring_fs/mod.rs
+++ fuse/src/uring_fuse/uring_fs/mod.rs
@@ -1,29 +1,28 @@
 use std::ffi::OsStr;
 use std::path::PathBuf;
+use std::sync::atomic::{AtomicU64, Ordering};
 use std::time::{Duration, SystemTime};
 
-use libc::ENOENT;
+use libc::{ENOENT, ENOSYS};
 use log::debug;
-use crate::uring_fuse::file_meta::FileType;
 
+use crate::uring_fuse::file_meta::FileType;
 use crate::uring_fuse::filesystem::Filesystem;
 use crate::uring_fuse::reply::reply_attr::ReplyAttr;
 use crate::uring_fuse::reply::reply_entry::ReplyEntry;
-use crate::uring_fuse::reply::reply_ops::{ReplyCreate, ReplyDirectory};
+use crate::uring_fuse::reply::reply_ops::{ReplyCreate, ReplyDirectory, ReplyWrite};
 use crate::uring_fuse::request::Request;
 use crate::uring_fuse::TimeOrNow;
-use crate::uring_fuse::uring_fs::inode:: InodeManager;
+use crate::uring_fuse::uring_fs::inode::InodeManager;
 use crate::uring_fuse::uring_fs::list_cache::ListStatusCache;
 
 pub mod inode;
 pub mod list_cache;
 
-// const NUMFILES: u8 = 16;
-// const MAXBYTES: u64 = 10;
-
 pub struct UringFilesystem {
     inode_manager: InodeManager,
     ls_cache: ListStatusCache,
+    next_fh: AtomicU64,
 }
 
 impl UringFilesystem {
@@ -31,6 +30,7 @@ impl UringFilesystem {
         Self {
             inode_manager,
             ls_cache,
+            next_fh: AtomicU64::new(1),
         }
     }
 }
@@ -53,94 +53,9 @@ impl Filesystem for UringFilesystem {
             }
         }
     }
-
     fn getattr(&mut self, _req: &Request, ino: u64, reply: ReplyAttr) {
         match self.inode_manager.get(ino) {
             Ok(inode) => reply.attr(&Duration::from_millis(100000), &inode.into()),
-            Err(_) => reply.error(ENOENT)
-        }
-    }
-
-    fn readdir(
-        &mut self,
-        _req: &Request,
-        ino: u64,
-        _fh: u64,
-        offset: i64,
-        mut reply: ReplyDirectory,
-    ) {
-        if let Ok(dir_inode) = self.inode_manager.get(ino) {
-            let mut entry_count = 0;
-            if offset == 0 {
-                let _ = reply.add(ino, 1,
-                          FileType::Directory, ".");
-                entry_count += 1;
-            }
-
-            if let Some(entries) = self.ls_cache.get(dir_inode.full_path()) {
-                let entries = entries.iter().enumerate().skip(offset as usize);
-                for (i, entry) in entries {
-                    match self.inode_manager.lookup(ino, entry) {
-                        Ok(entry_inode) => {
-                            // Cast i to i64 to match the offset type.
-                            let _ = reply.add(entry_inode.ino(), (i as i64) + offset + 2,
-                                      entry_inode.kind(), &entry_inode.name());
-                            entry_count += 1;
-                        }
-                        Err(_) => {
-                            // Handle the error, e.g., by logging or breaking the loop.
-                            // For now, we just continue to the next entry.
-                            continue;
-                        }
-                    }
-                }
-            } else {
-                // TODO: reload dir if ls_cache does not have the entry.
-
-                //reply.error(ENOENT)
-            }
-            if entry_count > 0 {
-                reply.ok()
-            } else {
-                reply.error(ENOENT)
-            }
-        } else {
-            // Handle the error if inode_manager does not have the inode.
-            reply.error(ENOENT)
-        }
-
-    }
-
-    fn create(
-        &mut self,
-        _req: &Request<'_>,
-        parent: u64,
-        name: &OsStr,
-        mode: u32,
-        _umask: u32,
-        flags: i32,
-        reply: ReplyCreate,
-    ) {
-        debug!(
-            "create: {:?}/{:?} (mode={:#o}, flags={:#x})",
-            parent, name, mode, flags
-        );
-        match self.inode_manager.get(parent) {
-            Ok(parent_node) => {
-                let full_path = PathBuf::from(parent_node.full_path()).join(name)
-                    .into_os_string().into_string().unwrap();
-                match self.inode_manager.create(parent,
-                                                name.to_os_string().into_string().unwrap(),
-                                                full_path,
-                                                FileType::RegularFile) {
-                    Ok(inode) => {
-                        let ino = inode.ino();
-                        self.ls_cache.append(String::from(parent_node.full_path()), name.to_os_string().into_string().unwrap());
-                        reply.created(&Duration::from_millis(1000000), &inode.into(), ino, 5, 0)
-                    }
-                    Err(_) => reply.error(ENOENT)
-                }
-            }
             Err(_) => reply.error(ENOENT)
         }
     }
@@ -169,5 +84,119 @@ impl Filesystem for UringFilesystem {
             ino, mode, uid, gid, size, fh, flags
         );
         reply.attr(&Duration::from_millis(1000000), &self.inode_manager.get(ino).unwrap().into());
+    }
+
+    fn write(
+        &mut self,
+        _req: &Request<'_>,
+        ino: u64,
+        fh: u64,
+        offset: i64,
+        data: &[u8],
+        write_flags: u32,
+        flags: i32,
+        lock_owner: Option<u64>,
+        reply: ReplyWrite,
+    ) {
+        debug!(
+            "write(ino: {:#x?}, fh: {}, offset: {}, data.len(): {}, \
+            write_flags: {:#x?}, flags: {:#x?}, lock_owner: {:?})",
+            ino,
+            fh,
+            offset,
+            data.len(),
+            write_flags,
+            flags,
+            lock_owner
+        );
+
+        reply.error(ENOSYS);
+    }
+
+    fn readdir(
+        &mut self,
+        _req: &Request,
+        ino: u64,
+        _fh: u64,
+        offset: i64,
+        mut reply: ReplyDirectory,
+    ) {
+        if let Ok(dir_inode) = self.inode_manager.get(ino) {
+            let mut entry_count = 0;
+            if offset == 0 {
+                let _ = reply.add(ino, 1,
+                                  FileType::Directory, ".");
+                entry_count += 1;
+            }
+
+            if let Some(entries) = self.ls_cache.get(dir_inode.full_path()) {
+                let entries = entries.iter().enumerate().skip(offset as usize);
+                for (i, entry) in entries {
+                    match self.inode_manager.lookup(ino, entry) {
+                        Ok(entry_inode) => {
+                            // Cast i to i64 to match the offset type.
+                            let _ = reply.add(entry_inode.ino(), (i as i64) + offset + 2,
+                                              entry_inode.kind(), &entry_inode.name());
+                            entry_count += 1;
+                        }
+                        Err(_) => {
+                            // TODO:Handle the error, e.g., by logging or breaking the loop.
+                            // For now, we just continue to the next entry.
+                            continue;
+                        }
+                    }
+                }
+            } else {
+                // TODO: reload dir if ls_cache does not have the entry.
+
+                //reply.error(ENOENT)
+            }
+            if entry_count > 0 {
+                reply.ok()
+            } else {
+                reply.error(ENOENT)
+            }
+        } else {
+            // Handle the error if inode_manager does not have the inode.
+            reply.error(ENOENT)
+        }
+    }
+
+    fn create(
+        &mut self,
+        _req: &Request<'_>,
+        parent: u64,
+        name: &OsStr,
+        mode: u32,
+        _umask: u32,
+        flags: i32,
+        reply: ReplyCreate,
+    ) {
+        debug!(
+            "create: {:?}/{:?} (mode={:#o}, flags={:#x})",
+            parent, name, mode, flags
+        );
+        match self.inode_manager.get(parent) {
+            Ok(parent_node) => {
+                let full_path = PathBuf::from(parent_node.full_path()).join(name)
+                    .into_os_string().into_string().unwrap();
+                match self.inode_manager.create(parent,
+                                                name.to_os_string().into_string().unwrap(),
+                                                full_path,
+                                                FileType::RegularFile) {
+                    Ok(inode) => {
+                        let ino = inode.ino();
+                        self.ls_cache.append(String::from(parent_node.full_path()), name.to_os_string().into_string().unwrap());
+                        reply.created(&Duration::from_millis(1000000),
+                                      &inode.into(),
+                                      ino,
+                                      self.next_fh.fetch_add(1, Ordering::Relaxed),
+                                      0)
+                    }
+                    Err(_) => reply.error(ENOENT)
+                }
+            }
+            Err(_) => reply.error(ENOENT)
+        }
     }
 }
